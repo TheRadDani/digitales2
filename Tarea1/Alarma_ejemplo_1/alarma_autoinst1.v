8385e43f6313840be32960d82450787f94a33304
TheRadDani <ldanielfch@gmail.com>
2021-08-30 16:52:31 -0600

    ffr

---
+++ Tarea1/Alarma_ejemplo_1/alarma_autoinst1.v
@@ -0,0 +1,13 @@
+`include "alarma_desc_conductual.v"
+
+module Newmodule;
+    
+    alarma_desc_conductual alarma_desc_conductual(/*AUTOINST*/
+						  // Outputs
+						  .sAlr			(sAlr),
+						  // Inputs
+						  .sLuz			(sLuz),
+						  .sPrta		(sPrta),
+						  .sIgn			(sIgn));
+
+endmodule
